6fe2633e68d599e821e3f52b25e75817a68ef763
kingradas <98946296+RadimPokorny@users.noreply.github.com>
2023-03-31 12:21:35 +0200

    Přidejte soubory projektu.

---
+++ HerniPostava.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hra
+{
+    internal class HerniPostava
+    {
+        public string _jmeno;
+        public string Jmeno
+        {
+            get { return _jmeno; }
+            set { 
+                _jmeno = value;
+
+                if (_jmeno.Length > 10)
+                {
+                    MessageBox.Show("Příliš dlouhé jméno!");
+                }
+
+            }
+        }
+
+        public int _level;
+        public int _poziceX;
+        public int _poziceY;
+
+        public HerniPostava(string jmeno)
+        {
+            _level = 1;
+            _poziceX = 0;
+            _poziceY = 0;
+            Jmeno = jmeno;
+        }
+
+        public void ZmenaPozice(bool upDown, bool rightLeft)
+        {
+            if (upDown == true)
+            {
+                _poziceY++;
+            }
+
+            if (rightLeft == true)
+            {
+                _poziceX++;
+            }
+        }
+        public override string ToString()
+        {
+            return Jmeno + " - level: " + _level + ", poziceX: " + _poziceX + ", poziceY: " + _poziceY;
+        }
+
+    }
+}
---
+++ Hrac.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hra
+{
+
+    public enum Trida
+    {
+        Kouzelnik,
+        Berserker,
+        Inzenyr,
+        Cizak
+    }
+
+    public enum Oblicej
+    {
+        velkynos,
+        usoplesk,
+        makeup
+    }
+
+    public enum Vlasy
+    {
+        drdol,
+        culik,
+        pleska
+    }
+
+    public enum BarvaVlasu
+    {
+        kastanova,
+        blond,
+        cervena
+    }
+
+
+    internal class Hrac : HerniPostava
+    {
+
+        
+
+        private Trida _trida;
+
+        
+
+        private Oblicej _oblicej;
+        
+        private Vlasy _vlasy;
+        
+        private BarvaVlasu _barvaVlasu;
+
+        public int _xp;
+
+        public Hrac(string jmeno, Trida trida, Oblicej oblicej, Vlasy vlasy, BarvaVlasu barvaVlasu ) : base(jmeno)
+        {
+            _xp = 0;
+            _oblicej = oblicej;
+            _vlasy = vlasy;
+            _barvaVlasu = barvaVlasu;
+            _trida = trida;
+        }
+
+        public void pridejXP()
+        {
+            _xp += 10;
+            if (_xp % 100 == 0)
+            {
+                _level++;
+            }
+        }
+
+
+    }
+}
