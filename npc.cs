047c6f13050c52f2fc62c636e18633d80ebfbdfc
simondockal <101214260+simondockal@users.noreply.github.com>
2023-03-31 12:28:36 +0200

    npc

---
+++ npc.cs
@@ -0,0 +1,31 @@
+﻿using Hra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace npc
+{
+    public enum Ocupation
+    {
+        obchodnik,
+        nepritel,
+        obyvatel,
+        korporatnikrysa,
+        prijemcePasivnihoPrijmuZNft
+    }
+
+    internal class Npc : HerniPostava
+    {
+        public Ocupation _octupation;
+
+        public Npc(Ocupation octupation,string jmeno) : base(jmeno)
+        {
+            _octupation = octupation;
+        }
+    }
+
+}
